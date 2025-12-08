bc9b11d868599ee9b06c0d5718c0844438bffd18
Ricardo Mello <ricardohsmello@gmail.com>
2025-12-08 15:41:43 -0300

    fix

--- src/main/java/org/example/springwithhibernatetestapp/SpringWithHibernateMongoTestAppApplication.java
+++ src/main/java/org/example/springwithhibernatetestapp/SpringWithHibernateMongoTestAppApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
 @EnableJpaRepositories(basePackages = "org.example.springwithhibernatetestapp")
 @EntityScan(basePackages = "org.example.springwithhibernatetestapp")
 public class SpringWithHibernateMongoTestAppApplication {
